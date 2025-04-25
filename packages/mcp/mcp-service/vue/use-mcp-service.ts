5a7a21dea8ff4bc0b84324fac7de7e6209ccff27
rhlin <r.h.lin@163.com>
2025-04-25 15:26:40 +0800

    feat(mcp): simple auto register tool for vue composition

--- packages/mcp/mcp-service/vue/use-mcp-service.ts
+++ packages/mcp/mcp-service/vue/use-mcp-service.ts
@@ -1,12 +1,23 @@
-import { inject, provide } from 'vue';
-import { MCP_SERVICE, McpService } from '@opentiny/tiny-agent-mcp-service';
+import { inject, onBeforeUnmount, onMounted, provide } from 'vue';
+import { MCP_SERVICE, McpService, ITool } from '@opentiny/tiny-agent-mcp-service';
 
 export function useMcpService() {
   const mcpService = inject(MCP_SERVICE) as McpService;
   if (!mcpService) {
     throw new Error('McpService not found');
   }
-  return mcpService;
+  const tool = (mcpTool: ITool) => {
+    onMounted(() => {
+      mcpService.registerTool(mcpTool);
+    });
+    onBeforeUnmount(() => {
+      mcpService.unregisterTool(mcpTool.name);
+    });
+  }
+  return {
+    mcp: mcpService,
+    tool
+  };
 }
 
 export function setupMcpService() {
