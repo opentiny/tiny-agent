1ef890645f00cee0d62a292c56813964f24b31de
gimmyhehe <975402925@qq.com>
2025-05-17 01:30:13 +0800

    fix: change agent serve

--- packages/mcp/mcp-agent/index.js
+++ packages/mcp/mcp-agent/index.js
@@ -1,28 +1,35 @@
-import express from 'express';
-import dotenv from 'dotenv';
-import { createMCPClient } from '../mcp-client-core/dist/index.js';
-import cors from 'cors';
+import express from "express";
+import dotenv from "dotenv";
+import { createMCPClient } from "../mcp-client-core/dist/index.js";
+import cors from "cors";
 
 dotenv.config();
 
-const main = async () => {
-  const client = await createMCPClient({
-    llmConfig: {
-      apiKey: process.env.OPEN_ROUTER_API_KEY,
-      model: process.env.OPEN_ROUTER_MODEL,
-      systemPrompt: '使用中文进行交流。',
-      iterationCount: 3,
-    },
-    mcpServerConfig: {
-      url: 'http://localhost:3005/sse?client=355748d-8f49-4e7b-8f5a-2ba44dcc5cda',
-    },
-  });
+const clientMap = new Map();
 
+const main = async () => {
   const app = express();
   app.use(cors());
   app.use(express.json());
 
-  app.post('/chat', async (req, res) => {
+  app.post("/chat", async (req, res) => {
+    const clientId = req.body.clientId;
+    let client = clientMap.get(clientId);
+    if (!client) {
+      client = await createMCPClient({
+        llmConfig: {
+          apiKey: process.env.OPEN_ROUTER_API_KEY,
+          model: process.env.OPEN_ROUTER_MODEL,
+          systemPrompt: "使用中文进行交流。",
+          iterationCount: 3,
+        },
+        mcpServerConfig: {
+          url: `http://localhost:3005/sse?client=${clientId}`,
+        },
+      });
+      clientMap.set(clientId, client);
+    }
+
     const response = await client.processQuery(req.body.query);
 
     // 这里可以接入你的 chat 逻辑
