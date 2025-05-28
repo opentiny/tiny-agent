b95aed1fabda4b7839587264921d661d9c7c4bc4
rhlin <r.h.lin@163.com>
2025-05-28 11:04:17 +0800

    build: fix useMcpService build type error

--- packages/mcp/mcp-service/vue/src/use-mcp-service.ts
+++ packages/mcp/mcp-service/vue/src/use-mcp-service.ts
@@ -1,13 +1,14 @@
 import { inject, onBeforeUnmount, onMounted, provide } from 'vue';
 import { MCP_SERVICE, McpService } from '@opentiny/tiny-agent-mcp-service';
-import { McpServer } from '@modelcontextprotocol/sdk/server/mcp';
 
-export function useMcpService() {
+export type DynamicToolFn = (...mcpTool: Parameters<McpService['registerTool']>) => void
+
+export function useMcpService(): { mcp: McpService, tool: DynamicToolFn} {
   const mcpService = inject(MCP_SERVICE) as McpService;
   if (!mcpService) {
     throw new Error('McpService not found');
   }
-  const tool = (...mcpTool: Parameters<McpServer['tool']>) => {
+  const tool = (...mcpTool: Parameters<McpService['registerTool']>) => {
     onMounted(() => {
       mcpService.registerTool(...mcpTool);
     });
