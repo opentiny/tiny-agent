006a1e048fe111d304f811ee02419a84348243d0
rhlin <r.h.lin@163.com>
2025-06-05 19:24:01 +0800

    refactor(demo):  refactor App.vue

--- demo/src/App.vue
+++ demo/src/App.vue
@@ -1,36 +1,22 @@
 <script setup>
 import { ref } from 'vue';
-import { setupMcpService } from '@opentiny/tiny-agent-mcp-service-vue';
-import { McpValidator } from '@opentiny/tiny-agent-mcp-service';
-import AiChatSvg from './assets/ai-chat.svg?url';
-import { EndpointTransport, WebSocketClientEndpoint } from '@opentiny/tiny-agent-mcp-connector';
-import { McpToolParser } from '@opentiny/tiny-agent-task-mcp';
 import ChatDialog from './components/ChatDialog.vue';
 import AddUser from './components/AddUser.vue';
-import mcpToolJson from './mcp-tool.json';
-import { taskScheduler } from './scheduler.js';
-const doTask = async (task) => {
-  return taskScheduler.pushTask(task);
-};
-const mcpValidator = new McpValidator();
-const mcp = setupMcpService();
-const wsEndpoint = new WebSocketClientEndpoint({ url: 'ws://localhost:3001/ws' });
-const endpointTransport = new EndpointTransport(wsEndpoint);
-mcp.mcpServer.connect(endpointTransport);
-mcp.setValidator(mcpValidator);
-new McpToolParser(doTask).extractAllTools(mcpToolJson).forEach((tool) => {
-  mcp.mcpServer.registerTool(tool.name, tool.config, tool.cb);
-});
+import AiChatSvg from './assets/ai-chat.svg?url';
+import { initMcp } from './mcp';
+
+const { endpointTransport, mcpValidator } = initMcp();
 
 const clientId = ref(endpointTransport.clientId);
-
-if (!endpointTransport.clientId) {
-  endpointTransport.clientIdResolved.then((id) => {
-    clientId.value = id;
+if (process.env.NODE_ENV === 'development') {
+  if (!endpointTransport.clientId) {
+    endpointTransport.clientIdResolved.then((id) => {
+      clientId.value = id;
+      console.log('Client ID:', clientId.value);
+    });
+  } else {
     console.log('Client ID:', clientId.value);
-  });
-} else {
-  console.log('Client ID:', clientId.value);
+  }
 }
 
 const showChat = ref(true);
---
+++ demo/src/mcp/index.js
@@ -0,0 +1,40 @@
+import { EndpointTransport, WebSocketClientEndpoint } from '@opentiny/tiny-agent-mcp-connector';
+import { McpValidator } from '@opentiny/tiny-agent-mcp-service';
+import { setupMcpService } from '@opentiny/tiny-agent-mcp-service-vue';
+import { McpToolParser } from '@opentiny/tiny-agent-task-mcp';
+import { useTaskScheduler } from "./scheduler";
+import mcpToolJson from './mcp-tool.json';
+
+export function initMcp() {
+  // Connector
+  const wsEndpoint = new WebSocketClientEndpoint({ url: import.meta.env.VITE_CONNECTOR_ENDPOINT_URL });
+  const endpointTransport = new EndpointTransport(wsEndpoint);
+  
+  // MCP Service
+  const mcpService = setupMcpService();
+  mcpService.mcpServer.connect(endpointTransport);
+
+  // MCP Validator
+  const mcpValidator = new McpValidator();
+  mcpService.setValidator(mcpValidator);
+
+  // Task Scheduler
+  const {taskScheduler, actionManager} = useTaskScheduler();
+  const doTask = async (task) => taskScheduler.pushTask(task);
+  
+  // MCP Tool Parser & mcp-tool.json
+  const ncpToolParser = new McpToolParser(doTask);
+  ncpToolParser.extractAllTools(mcpToolJson).forEach((tool) => {
+    mcpService.mcpServer.registerTool(tool.name, tool.config, tool.cb);
+  });
+
+  return {
+    wsEndpoint,
+    endpointTransport,
+    mcpService,
+    mcpValidator,
+    taskScheduler,
+    actionManager,
+    ncpToolParser
+  }
+}
