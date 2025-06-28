fedc459e32073a927e69b45c75bfb883aae7025a
yy-wow <yaoyun_bigger@outlook.com>
2025-06-28 16:53:26 +0800

    feat: private deploy vis

--- demo/src/components/chat-config/chat-config.js
+++ demo/src/components/chat-config/chat-config.js
@@ -40,4 +40,15 @@ export const promptItems = [
     icon: h('span', { style: { fontSize: '18px' } }, '🧠'),
     badge: 'NEW',
   },
+  {
+    label: '生成图表',
+    description:`请生成一个饼图，具体为环图，边缘圆角，展示各平台的成本。数据列在右方，中间展示成本总计数，下方展示"总计(元)"，
+- 淘乐电商网站 900人民币
+- 淘乐本地生活 500人民币
+- 淘乐粉论坛 200人民币
+- 企业品牌网站 100人民币
+- 其他 34.9人民币`,
+    icon: h('span', { style: { fontSize: '18px' } }, '🧠'),
+    badge: 'NEW',
+  },
 ];
